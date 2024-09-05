7834c7c3d849a9151f8a2b6dc91cc6015e165187
Reisen <mauriciopastana884@gmail.com>
2024-09-05 06:52:30 -0300

    Added Scene Select

--- js/plugins/Kamigami_Campaign_Select.js
+++ js/plugins/Kamigami_Campaign_Select.js
@@ -11,9 +11,11 @@ Scene_Kamigami_CampaignSelect.prototype.initialize = function () {
     this.createVariables();
     this.createBackMap();
     this.createBackFade();
+    this.createGod();
     this.createBackOptions();
     this.createButtons();
     this.createDescriptions();
+    this.createSelectionButtons();
 };
 //-----------------------------------------------------------------------------
 // Function : updates - updates process
@@ -21,6 +23,8 @@ Scene_Kamigami_CampaignSelect.prototype.initialize = function () {
 Scene_Kamigami_CampaignSelect.prototype.createVariables = function () {
     this.index = 0;
     this.phase = 0;
+    this.configHue = [0, 260, 40, 90, 310];
+    this.currentHue = 0;
 }
 //-----------------------------------------------------------------------------
 // Function : updates - updates process
@@ -38,12 +42,22 @@ Scene_Kamigami_CampaignSelect.prototype.createBackMap = function () {
 //-----------------------------------------------------------------------------
 // Function : updates - updates process
 //-----------------------------------------------------------------------------
+Scene_Kamigami_CampaignSelect.prototype.createGod = function () {
+    this._bigGod = new Sprite();
+    this._bigGod.bitmap = ImageManager.loadFace("ZeusF");
+    this.addChild(this._bigGod);
+}
+
+//-----------------------------------------------------------------------------
+// Function : createGod
+//-----------------------------------------------------------------------------
 Scene_Kamigami_CampaignSelect.prototype.createBackFade = function () {
     this._backFade = new Array();
     this._backFade = new Sprite();
     this._backFade.bitmap = ImageManager.loadCampaign("frontEffect");
     this.addChild(this._backFade);
 }
+
 //-----------------------------------------------------------------------------
 // Function : create
 //-----------------------------------------------------------------------------
@@ -108,7 +122,8 @@ Scene_Kamigami_CampaignSelect.prototype.createDescriptions = function () {
     this._descriptionMythology.text = IAVRA.I18N.localize("#{DuelVocab.Campaign.japanStory}")
     this.addChild(this._descriptionMythology);
     this._descriptionMythology.y = 100
-    this._descriptionMythology.x = 1920 - 360
+    this._descriptionMythology.x = 1920 - 20
+    this._descriptionMythology.anchor.x = 1;
 
     this._descriptionHeader2 = new PIXI.Text("", { fontFamily: 'GameFont', fontSize: 48, fill: 0xffffff, align: 'left'});
     this._descriptionHeader2.text = IAVRA.I18N.localize("#{DuelVocab.Campaign.preCampaignStrategy}")
@@ -122,9 +137,40 @@ Scene_Kamigami_CampaignSelect.prototype.createDescriptions = function () {
     this._descriptionStrategy.text = IAVRA.I18N.localize("#{DuelVocab.Campaign.japanStrategy}")
     this.addChild(this._descriptionStrategy);
     this._descriptionStrategy.y = 680
-    this._descriptionStrategy.x = 1920 - 360
+    this._descriptionStrategy.x = 1920 - 20
+    this._descriptionStrategy.anchor.x = 1;
     //this._descriptionMythology.alpha = 0
 }
+//-----------------------------------------------------------------------------
+// Function : createSelectionButtons
+//-----------------------------------------------------------------------------
+Scene_Kamigami_CampaignSelect.prototype.createSelectionButtons = function () {
+    this._selectButtons = new Array();
+    let symbols = ["Greek", "Egypt", "Norse", "Japan", "Brazil"]
+    for (let n = 0; n < 5; n++) {
+        this._selectButtons[n] = new Sprite();
+        this._selectButtons[n].bitmap = ImageManager.loadCampaign("selectedMito");
+        this._selectButtons[n].anchor.x = 0.5;
+        this._selectButtons[n].anchor.y = -2.5;
+        this._selectButtons[n].x = Graphics.width/2;
+        this._selectButtons[n].y = Graphics.height/2;
+        this._selectButtons[n].rotation = (2 - n) * 0.85;
+        this._selectButtons[n].mythoText = new PIXI.Text("", { fontFamily: 'GameFont', fontSize: 48, fill: 0xffffff, align: 'center'});
+        this._selectButtons[n].mythoText.y = 420
+        this._selectButtons[n].mythoText.alpha = 0.7
+        this._selectButtons[n].mythoText.anchor.x = 0.5;
+        this._selectButtons[n].addChild(this._selectButtons[n].mythoText)
+        this._selectButtons[n].mythoText.text = IAVRA.I18N.localize(`#{DuelVocab.Campaign.mythology${n + 1}}`)
+        this._selectButtons[n].mythoSymbol = new Sprite();
+        this._selectButtons[n].mythoSymbol.bitmap = ImageManager.loadCampaign(symbols[n]);
+        this._selectButtons[n].mythoSymbol.anchor.x = 0.5;
+        this._selectButtons[n].mythoSymbol.y = 480
+        this._selectButtons[n].addChild(this._selectButtons[n].mythoSymbol)
+        this.addChild(this._selectButtons[n])
+        this._selectButtons[n].setHue(this.configHue[n])
+    }
+}
+
 //-----------------------------------------------------------------------------
 // Function : updates - updates process
 //-----------------------------------------------------------------------------
